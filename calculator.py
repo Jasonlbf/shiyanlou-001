67cf76418d65c7b297791b56a3a1dd90d221d7e7
Jasonlbf <jasonlbf@gmail.com>
2018-12-18 00:41:36 +0800

    modify calculator

--- calculator.py
+++ calculator.py
@@ -2,30 +2,45 @@
 
 import sys
 
-salary = 0
-try:
-    salary = int(sys.argv[1])
-except:
-    print('Parameter Error')
-    exit()
+def getInfo(infos):
+    dic = {}
+    for i in infos:
+        lis = i.split(':')
+        dic[lis[0]] = lis[1]
 
-tax_amount = salary - 3500
+    return dic
 
-if tax_amount <= 0:
-    amount = 0
-elif tax_amount <= 1500:
-    amount = tax_amount * 0.03
-elif tax_amount <= 4500:
-    amount = tax_amount * 0.1 - 105
-elif tax_amount <= 9000:
-    amount = tax_amount * 0.2 - 555
-elif tax_amount <= 935000:
-    amount = tax_amount * 0.25 - 1005
-elif tax_amount <= 55000:
-    amount = tax_amount * 0.3 - 2755
-elif tax_amount <= 80000:
-    amount = tax_amount * 0.35 - 5505
-else:
-    amount = tax_amount * 0.45 - 13505
 
-print(format(amount,'.2f'))
+def afterSalary(salary):
+    tax = salary * 0.08 + salary * 0.02 + salary * 0.005 + salary * 0.06 
+    tax_amount = salary - tax - 3500
+     
+    if tax_amount <= 0:
+        amount = 0
+    elif tax_amount <= 1500:
+        amount = tax_amount * 0.03
+    elif tax_amount <= 4500:
+        amount = tax_amount * 0.1 - 105
+    elif tax_amount <= 9000:
+        amount = tax_amount * 0.2 - 555
+    elif tax_amount <= 935000:
+        amount = tax_amount * 0.25 - 1005
+    elif tax_amount <= 55000:
+        amount = tax_amount * 0.3 - 2755
+    elif tax_amount <= 80000:
+        amount = tax_amount * 0.35 - 5505
+    else:
+        amount = tax_amount * 0.45 - 13505
+    
+    return salary - tax - amount
+
+
+if __name__ == '__main__':
+
+    infos = getInfo(sys.argv[1:])
+    
+    for uid,salary in infos.items():
+        try:
+            print(int(uid),':',format(afterSalary(int(salary)),'.2f'))
+        except:
+            print('Parameter Error')
