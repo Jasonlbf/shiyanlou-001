4e1a968f7ddb9b185526a579afb751b4045872fb
LIUBF <jasonlbf@gmail.com>
2018-12-26 17:40:22 +0800

    update

--- news/app.py
+++ news/app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# -*- coding:utf-8-*-
 
 import os
 import json
@@ -22,6 +23,13 @@ class File(db.Model):
     created_time = db.Column(db.DateTime)
     category_id = db.Column(db.Integer,db.ForeignKey('category.id'))
     content = db.Column(db.Text)
+    category = db.relationship('Category')
+
+    def __init__(self,title,created_time,category,content):
+        self.title  = title
+        self.created_time = created_time
+        self.content = content
+        self.category = category
 
     def __repr__(self):
         return '<File(title=%s)>' % self.title
@@ -29,18 +37,31 @@ class File(db.Model):
 class Category(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(80))
+    def __init__(self,name):
+        self.name = name
+    def __repr__(self):
+        return '<Category(name=%s)>' % self.name
 
 @app.route('/')
 def index():
     #show books list
 #    return hs['title'] + ' ' +  hw['title']
-    return ''
+    files = File.query.all()
+    file_titles = {} 
+    for fil in files:
+        file_titles[fil.id] = fil.title
+    return render_template('index.html',file_titles=file_titles)
 
-@app.route('/files/<filename>')
-def file(filename):
+@app.route('/files/<file_id>')
+def file(file_id):
     #reader content
 #    return reader(filename)['content']
-    return ''
+    fileobj = File.query.filter_by(id=file_id).first()
+    file_list = [fileobj.content,str(fileobj.created_time),fileobj.category.name]
+#    for i in iter(file_list):
+#        print(i)
+    return render_template('file.html',file_list=file_list)
+
 
 @app.errorhandler(404)
 def not_found(error):
