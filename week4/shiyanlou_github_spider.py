1d9b2473edcd0550eb6eddaf61705c6658a18e9c
jasonlbf@gmail.com <jasonlbf@gmail.com>
2019-01-06 15:57:44 +0800

    spider github

---
+++ week4/shiyanlou_github_spider.py
@@ -0,0 +1,22 @@
+# -*= coding:utf-8 -*-
+import scrapy
+
+class ShiyanlouGithubSpider(scrapy.Spider):
+    name = 'shiyanlou-github'
+
+    @property
+    def start_urls(self):
+        urls = [
+                'https://github.com/shiyanlou?tab=repositories',
+                'https://github.com/shiyanlou?after=Y3Vyc29yOnYyOpK5MjAxNy0wNi0wN1QwNjoyMToxMCswODowMM4FkpVn&tab=repositories',
+                'https://github.com/shiyanlou?after=Y3Vyc29yOnYyOpK5MjAxNS0wMS0yNlQxMTozMDoyNSswODowMM4Bx2JQ&tab=repositories',
+                'https://github.com/shiyanlou?after=Y3Vyc29yOnYyOpK5MjAxNC0xMS0yMVQxODowOTowMiswODowMM4BnQBZ&tab=repositories'
+                ]
+        return urls 
+    
+    def parse(self,response):
+        for course in response.css('div.col-9.d-inline-block'):
+            yield {
+                    'name': course.css('div.d-inline-block.mb-1 a::text').re_first('[^\w]*(\w+)[^\w]*'),
+                    'update_time': course.css('div.f6.text-gray.mt-2 relative-time::attr(datetime)').extract_first()
+                    }
