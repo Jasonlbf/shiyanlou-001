9e144c4fd2125c59289c2191fa862889d6dd382c
LIUBF <jasonlbf@gmail.com>
2019-01-11 18:24:17 +0800

    tz19-1

--- week5/tiaozhan19/flask_doc/flask_doc/spiders/flask.py
+++ week5/tiaozhan19/flask_doc/flask_doc/spiders/flask.py
@@ -4,19 +4,19 @@ from scrapy.spiders import Rule
 from scrapy.linkextractors import LinkExtractor
 from flask_doc.items import PageItem
 
-class FlaskSpider(scrapy.Spider):
+class FlaskSpider(scrapy.spiders.CrawlSpider):
     name = 'flask'
     allowed_domains = ['flask.pocoo.org']
     start_urls = ['http://flask.pocoo.org/docs/1.0']
 
     rules = (
-            Rule(),
+            Rule(link_extractor=LinkExtractor()),
             )
 
     def parse(self, response):
         pass
 
-    def parse(self, response):
+    def parse_page(self, response):
         item = PageItem()
-
+        #item['url'] = response
         yield item
