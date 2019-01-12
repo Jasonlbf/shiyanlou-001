f98275e4e7cf1970aae604b6fb6871f4e3373fa6
LIUBF <jasonlbf@gmail.com>
2019-01-12 10:54:54 +0800

    112

--- week5/tiaozhan19/flask_doc/flask_doc/spiders/flask.py
+++ week5/tiaozhan19/flask_doc/flask_doc/spiders/flask.py
@@ -8,9 +8,9 @@ class FlaskSpider(scrapy.spiders.CrawlSpider):
     name = 'flask'
     allowed_domains = ['flask.pocoo.org']
     start_urls = ['http://flask.pocoo.org/docs/1.0']
-
+    links = LinkExtractor(allow="flask.pocoo.org/docs/1.0/*")
     rules = (
-            Rule(link_extractor=LinkExtractor()),
+            Rule(link_extractor=links,callback='parse_page',follow=True),
             )
 
     def parse(self, response):
